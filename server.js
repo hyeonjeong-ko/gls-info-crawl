3f1998a8a2fd5dd8e809860058d4378363779513
Ubuntu <ubuntu@ip-172-31-12-202.ap-northeast-2.compute.internal>
2023-11-13 16:05:32 +0000

    shim_branch_merge_to_serverjs

--- server.js
+++ server.js
@@ -110,6 +110,170 @@ app.get('/courses', (req, res) => {
   
 
 
+// =======================================================================================================
+
+
+
+//과목 추가, (front에서 data 입력 -> db에 추가) 
+// course_name을 유저가 검색해서 자기가 맞는 과목을 넣을 때 course_name이 여러개일 경우..
+app.post("/timetable/add", (req, res) => {
+  const course_name  = req.body.course_name;
+  const userID = req.body.userID;
+  // userID = 1
+  console.log(course_name);
+  // 과목을 courses 테이블에서 찾습니다.
+  const selectCourseQuery = `
+    SELECT *
+    FROM Courses
+    WHERE Course_name = ?
+  `;
+
+  db.all(selectCourseQuery, [course_name], (err, courseData) => {
+    if (err) {
+      return res.status(500).json({ message: "과목 조회 실패" });
+    }
+
+    if (!courseData || courseData.length === 0) {
+      return res.status(404).json({ message: "과목을 찾을 수 없음" });
+    }
+
+    // 만약 course_name이 여러 개일 경우
+    if (courseData.length > 1) {
+      res.json({
+        message: "동일한 과목이 여러 개 있습니다. 선택해 주세요.",
+        courseData: courseData
+      });
+    } else {
+      // 단일 과목인 경우
+      // 찾은 과목을 timetable 테이블에 추가합니다.
+      const course_id = courseData[0].course_ID;
+      const insertTimetableQuery = `
+        INSERT INTO Enrollments (student_ID , course_ID)
+        VALUES (?, ?)
+      `;
+
+      db.run(insertTimetableQuery, [userID, course_id], (err) => {
+        if (err) {
+          return res.status(500).json({ message: "과목 추가 실패" });
+        }
+
+        res.json({
+          message: "과목이 성공적으로 추가되었습니다.",
+          courseData: courseData
+        });
+      });
+    }
+  });
+});
+
+//course_name이 여러개여서 클라이언트 한테 요청을 보내고 fetch로 다시 돌아온다.
+//그 selectedcourse를 db에 저장하는데 여기서 customdata 저장하는 것도 이 app.post를 사용해도 괜찮을거 같다.
+//custom data를 받을 경우에는 유저가 직접 course_name, class_time, class_room 같은 내용을 직접 입력하는 란이 있다면.
+app.post("/timetable/addSelectedCourse", (req, res) => {
+  const {
+    userID, course_ID
+  } = req.body;
+  const insertQuery = `
+    INSERT INTO Enrollments (student_ID , course_ID)
+    VALUES (?, ?)
+  `;
+
+  db.run(
+    insertQuery,
+    [userID, course_ID],
+    function (err) {
+      if (err) {
+        return res.status(500).json({ message: "과목 추가 실패" });
+      }
+
+      res.json({ message: "과목이 성공적으로 추가되었습니다." });
+    }
+  );
+});
+
+
+// //과목 삭제, (front에서 요청이 옴 -> db에서 삭제)
+// app.delete("/timetable/delete", (req, res) => {
+//   const {userID, course_ID} = req.body;
+//   const deleteQuery = `
+//     DELETE FROM Enrollments
+//     WHERE student_ID = ?
+//     AND course_ID = ? `;
+
+//   db.run(deleteQuery, [userID, course_ID], function (err) {
+//     if (err) {
+//       return res.status(500).json({ message: "과목 삭제 실패" });
+//     }
+
+//     if (this.changes > 0) {
+//       res.json({ message: "과목이 삭제되었습니다." }); //db에 변경이 있을 경우 this.changes가 0보다 커진다. 따라서 삭제 된 걸 확인 할 수 있다.
+//     } else {
+//       res.status(404).json({ message: "과목을 찾을 수 없습니다." });
+//     }
+//   });
+// });
+
+
+// edit에 어떤것? 어차피 course_id가 바뀌는게 아니라면 db를 건들필요없는데 professor나 이름 같은거 바꾸는거는 db 바꿀 게 없으니 edit api
+// app.patch("/timetable/edit", (req, res) => {
+//   const updatedData = req.body;
+//   const courseName = updatedData.course_name;
+//   const classTime = updatedData.class_time;
+
+//   const updateQuery = `
+//     UPDATE timetable
+//     SET professor = ?,
+//         class_room = ?
+//     WHERE course_name = ? AND class_time = ?;
+//   `;
+
+//   db.run(
+//     updateQuery,
+//     [updatedData.professor, updatedData.class_room, courseName, classTime],
+//     function (err) {
+//       if (err) {
+//         return res.status(500).json({ message: "과목 업데이트 실패" });
+//       }
+
+//       res.json({ message: "과목이 성공적으로 업데이트되었습니다." });
+//     }
+//   );
+// });
+
+
+
+
+app.get("/timetable/search", (req, res) => {
+ 
+  const searchQuery = req.query.searchQuery; 
+
+
+  console.log("Received search query: ", searchQuery);
+  const selectQuery = `
+    SELECT *
+    FROM courses  
+    WHERE course_name LIKE ?;  
+  `;
+
+
+  const searchPattern = `%${searchQuery}%`; 
+
+
+  db.all(
+    selectQuery,
+    [searchPattern],  
+    (err, data) => {
+      if (err) {
+ 
+        return res.status(500).json({ message: "과목 검색 실패", error: err.message });
+      }
+
+      res.json(data);
+    }
+  );
+});
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
